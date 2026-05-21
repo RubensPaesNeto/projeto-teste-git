d1e87f7194093dd624a67a64deb625107d3bced9
Rubens Paes Neto <rubenspaesneto@gmail.com>
2026-05-21 14:48:15 -0300

    feat: feito as funções de adição, multiplicação e soma

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function soma(a,b){
     return a + b;
 }
+function subtracao(a,b){
+    return a - b;
+}
+function multiplicacao(a,b){
+    return a * b;
+}
+
+console.log(subtracao(1,2))
 console.log(soma(1,2))
+console.log(multiplicacao(1,2))
